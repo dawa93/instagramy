2c740d6353097aaad15d8cc38640d8d116b3d6b2
Donghwan <dawa9293@gmail.com>
2026-02-10 00:11:25 +0900

    refactor: 좋아요 ux 개선(빠르게 반응하기)

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,15 +1,39 @@
 import useSWR, { useSWRConfig } from 'swr';
 import { SimplePost } from '../model/post';
 
+const updateLike = async (id: string, like: boolean) => {
+  return fetch('/api/likes', {
+    method: 'PUT',
+    body: JSON.stringify({ id, like }),
+  }).then(res => res.json());
+};
+
 export default function usePosts() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
-  const { mutate } = useSWRConfig();
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<SimplePost[]>('/api/posts');
 
   const setLike = (post: SimplePost, username: string, like: boolean) => {
-    fetch('/api/likes', {
-      method: 'PUT',
-      body: JSON.stringify({ id: post.id, like }),
-    }).then(() => mutate('/api/posts'));
+    const newPost = {
+      ...post,
+      likes: like
+        ? [...post.likes, username]
+        : post.likes.filter(item => item !== username),
+    };
+
+    const newPosts = posts?.map(post => {
+      return post.id === post.id ? newPost : post;
+    });
+
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    });
   };
 
   return { posts, isLoading, error, setLike };
