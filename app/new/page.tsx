c458d688533ea4501a24c5a020b5b836f2ae4f75
Hwanieee <dawa9293@gmail.com>
2026-03-23 19:57:47 +0900

    feat: new post 페이지 추가, 드래그 앤 드랍 추가 (#27)

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import NewPost from '@/src/components/NewPost';
+import { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 
-function Page() {
+export const metadata: Metadata = {
+  title: 'New post',
+  description: 'Create a new post',
+};
+
+async function Page() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect('/auth/signin');
+  }
+
   return (
     <div>
-      <div>new page</div>
+      <NewPost user={session.user} />
     </div>
   );
 }
