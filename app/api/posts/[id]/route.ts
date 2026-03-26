34c58b0f18662af5b0a4e2c33af04eae56f0227f
Hwanieee <dawa9293@gmail.com>
2026-03-26 14:23:14 +0900

    fix: posts get api 타입 수정 (#31)

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -3,14 +3,25 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getPost } from '@/src/service/posts';
 import { withSessionUser } from '@/src/utils/session';
 
-type Context = {
-  params: { id: string };
-};
-
-export async function GET(request: NextRequest, context: Context) {
+export async function GET(
+  request: NextRequest,
+  context: RouteContext<'/api/posts/[id]'>,
+) {
   return withSessionUser(async (user) => {
-    const id = (await context.params).id;
+    const { id } = await context.params;
 
     return getPost(id).then((data) => NextResponse.json(data));
   });
 }
+
+// 또는
+// type Context = {
+//   params: Promise<{ id: string }>;
+// };
+// export async function GET(request: NextRequest, context: Context) {
+//   return withSessionUser(async (user) => {
+//     const id = (await context.params).id;
+
+//     return getPost(id).then((data) => NextResponse.json(data));
+//   });
+// }
