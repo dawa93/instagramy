eece854fe6683fc6da8b9475be38c1682c8f4249
Hwanieee <dawa9293@gmail.com>
2026-03-26 14:36:41 +0900

    fix: 키워드 context 타입 수정, main푸시일때만 디플로이 활성화 추가 (#32)

--- app/api/search/[keyword]/route.ts
+++ app/api/search/[keyword]/route.ts
@@ -2,12 +2,22 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { searchUsers } from '@/src/service/user';
 
-interface Context {
-  params: { keyword: string };
-}
-
-export async function GET(_: NextRequest, context: Context) {
+export async function GET(
+  _: NextRequest,
+  context: RouteContext<'/api/search/[keyword]'>,
+) {
   const keyword = (await context.params).keyword;
 
   return searchUsers(keyword).then((data) => NextResponse.json(data));
 }
+
+// Or
+// interface Context {
+//   params: Promise<{ keyword: string }>;
+// }
+
+// export async function GET(_: NextRequest, context: Context) {
+//   const keyword = (await context.params).keyword;
+
+//   return searchUsers(keyword).then((data) => NextResponse.json(data));
+// }
